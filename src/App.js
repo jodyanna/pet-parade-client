677077efa03df29b2b1b83afcd79ddc02e8d232c
Joseph D'Anna <jodyanna14.15@gmail.com>
2021-08-22 14:00:03 -0500

    routing, log in and out func with user context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
+import React, {useState, useEffect} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import userContext from "./context/userContext";
 import './App.css';
 
+const initUserState = {
+  id: null,
+  username: "",
+  email: "",
+  city: "",
+  state: "",
+  dateCreated: null,
+  dateModified: null
+};
+
 function App() {
+  const [user, setUser] = useState(initUserState);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = user => {
+    setUser(user);
+    setIsLoggedIn(true);
+    localStorage.setItem("pet-parade-user-info", JSON.stringify(user));
+  }
+
+  const logout = () => {
+    setUser(initUserState);
+    setIsLoggedIn(false);
+    localStorage.removeItem("pet-parade-user-info");
+  }
+
+  useEffect(() => {
+    if (window.performance) {
+      if (performance.getEntriesByType("navigation")[0].type.localeCompare("reload") === 0) {
+        let parsedData = JSON.parse(localStorage.getItem("pet-parade-user-info"));
+
+        if(parsedData !== null) {
+          setUser(parsedData);
+          setIsLoggedIn(true);
+        }
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
+    <userContext.Provider value={{user: user, isLoggedIn: isLoggedIn, login: login, logout: logout}}>
+      <Router>
+      <div className="App">
       
+      <Switch>
+        <Route>
+          
+        </Route>
+      </Switch>
     </div>
+    </Router>
+    </userContext.Provider>
   );
 }
 
