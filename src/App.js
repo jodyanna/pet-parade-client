5d045aadc8aff986c394105fa1577d23ad9fe350
Joseph D'Anna <jodyanna14.15@gmail.com>
2021-10-20 13:01:20 -0500

    added route and component for 404 page not found

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import User from "./components/User";
 import Leaderboard from "./components/Leaderboard";
 import About from "./components/About";
 import Admin from "./components/Admin";
+import FourZeroFour from "./components/FourZeroFour";
 
 export default function App() {
   const [user, setUser] = useState(null);
@@ -78,6 +79,10 @@ export default function App() {
 
             <Route exact path="/">
               <Home user={user} />
+            </Route>
+
+            <Route path="*">
+              <FourZeroFour />
             </Route>
           </Switch>
         </div>
---
+++ src/components/FourZeroFour/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./index.module.css";
+
+export default function FourZeroFour() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.text}>404: Page does not exist</h2>
+    </div>
+  )
+}
